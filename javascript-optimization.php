ec8f3ab7a42f47515aec148ae391da00f91e0ef4
Shiv Yadav <shiv@cibirix.com>
2022-01-19 13:04:58 +0530

    js speed

--- javascript-optimization.php
+++ javascript-optimization.php
@@ -1,6 +1,8 @@
-google this : script type="lazyload_int"
-https://stackoverflow.com/questions/8946715/lazy-loading-javascript-and-inline-javascript
-https://www.websitepolicies.com : 
-https://developer.mozilla.org/en-US/docs/Web/Performance/Lazy_loading
-
-async and defer
+<ul>
+<li>google this : script type="lazyload_int"</li>
+<li>https://stackoverflow.com/questions/8946715/lazy-loading-javascript-and-inline-javascript</li>
+<li>https://www.websitepolicies.com : </li>
+<li>https://developer.mozilla.org/en-US/docs/Web/Performance/Lazy_loading</li>
+<li></li>
+<li>async and defer</li>
+</ul>
