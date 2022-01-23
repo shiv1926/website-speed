9cdbee96b742cb7cde229ccb0c47fc6da7b4f633
shivkumar yadav <shiv.yadav26@gmail.com>
2022-01-23 12:36:18 +0530

    inde

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		</ul>
 	</li>
 	<li>
-		<h4>Website Performance Testing Tools</h4>
+			<h4>Website Performance Testing Tools</h4>
 		<ul>
 			<li>Google page insighsts</li>
 			<li>Pingdom Tools</li>
@@ -23,6 +23,29 @@
 	</li>
 	<li><a href=""></a></li>
 	<li><a href="what-is-caching.php">What is caching</a></li>
+	dns-prefech
+async defer
+preloading
+what is cls
+
+lazy loading, how to lazy load images in background, 
+what is cdn and how to use cdn
+prefetch, preconnect, preload, crossorigin
+AMP pages important
+
+CSS ASPECT RATIO BOX : https://css-tricks.com/aspect-ratio-boxes/
+
+preloader / website preloader / website loader
+
+https://technumero.com/check-gzip-compression/
+https://technumero.com/keep-alive-test/
+
+use of picture/source tag instead of using direct image, with mulitple image sizes, : read this
+aviod excessive dom
+
+
+<script defer src="https://javascript.info/article/script-async-defer/long.js?speed=1"></script> , why add ?speed=1 
+
 	<li><a href="database-optimization.php">Database Optimization</a></li>
 	<li><a href="https://www.freenom.com/en/index.html?lang=en">to get free domain</a></li>
 	<li><a href="enable-text-compression.php">Enable text compression</a></li>
